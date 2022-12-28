141c01d1633a8a3037a7565484668df0d862a5d5
Tejas Nambiar <tejasnambiar32@gmail.com>
2022-12-28 11:59:35 +0530

    base app changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthenticationService } from './service/authentication/authentication.service';
+import { UserService } from './service/user/user.service';
+import { AuthInterceptor } from './interceptor/auth/auth.interceptor';
+import { AuthenticationGuard } from './gaurd/authentication.guard';
 
 @NgModule({
   declarations: [
@@ -10,9 +14,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    AuthenticationGuard,
+    AuthenticationService,
+    UserService,
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
